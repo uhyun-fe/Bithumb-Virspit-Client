59bbc97d9534d2d3e3b6a2d24318da5a3c4a3700
uhyun-fe <feyouhyun0957@gmail.com>
2021-09-23 15:11:04 +0900

    <update> signup page 이용약관 동의 관련 모달 관리 작업

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,19 @@
 import React from "react";
+
+// Styles
+import { Button, SpaceBetweenFlexDiv } from "../../assets/styles/basic.style";
 import { ModalArea } from "./Modal.style";
 
-const Modal = ({ width_percent }) => {
+const Modal = ({ max_width, title, contents, closing }) => {
    return (
-      <ModalArea>
-         <div className="content">modal</div>
+      <ModalArea width_percent={max_width}>
+         <div className="modal">
+            <SpaceBetweenFlexDiv className="top">
+               <h4>{title}</h4>
+               <Button onClick={closing}>X</Button>
+            </SpaceBetweenFlexDiv>
+            <div className="contents">{contents}</div>
+         </div>
       </ModalArea>
    );
 };
--- src/routes/SignupPage/Signup.logic.js
+++ src/routes/SignupPage/Signup.logic.js
@@ -2,20 +2,24 @@ import React, { useState } from "react";
 
 // Contents
 import pathname from "../../assets/contents/pathname";
+import terms from "../../assets/contents/terms";
 
 const SignupLogic = ({ history }) => {
    const [inputState, setInputState] = useState({
       email: "",
+      email_code: null,
+      is_right_email: false,
       name: "",
       pw: "",
       rePw: "",
       phone: "",
       birthday: "",
       gender: "",
-      virspit_term: false,
-      transaction_term: false,
-      privacy_term: false,
-      marketing_term: false,
+      virspit_term: { is_apply: false, is_essential: true, contents: null, title: "ViRSPiT 이용약관 동의" },
+      transaction_term: { is_apply: false, is_essential: true, contents: null, title: "전자금융거래 이용약관 동의" },
+      privacy_term: { is_apply: false, is_essential: true, contents: null, title: "개인정보 수집 및 이용 동의" },
+      marketing_term: { is_apply: false, is_essential: false, contents: null, title: "ViRSPiT 마케팅 정보 수신 동의" },
+      selected_term: null,
    });
 
    // Set InputState
@@ -35,9 +39,9 @@ const SignupLogic = ({ history }) => {
       if (!phone || phone.length < 12) return alert("연락처를 입력해주세요");
       if (!birthday || birthday.length < 10) return alert("생년월일을 입력해주세요");
       if (!gender) return alert("성별을 선택해주세요");
-      if (!virspit_term) return alert("ViRSPiT 이용약관에 동의해주세요");
-      if (!transaction_term) return alert("전자금융거래 이용약관에 동의해주세요");
-      if (!privacy_term) return alert("개인정보 수집 및 이용에 동의해주세요");
+      if (!virspit_term.is_apply) return alert("ViRSPiT 이용약관에 동의해주세요");
+      if (!transaction_term.is_apply) return alert("전자금융거래 이용약관에 동의해주세요");
+      if (!privacy_term.is_apply) return alert("개인정보 수집 및 이용에 동의해주세요");
       return true;
    };
 
@@ -53,6 +57,41 @@ const SignupLogic = ({ history }) => {
       // } catch (err) {
       //    console.error(err);
       // }
+   };
+
+   /* 이메일 인증 관련 */
+
+   // Send Email For Certification
+   const sendEmail = () => {
+      if (!inputState.email) return;
+      console.log("send email" + inputState.email);
+      setInputState({ ...inputState, email_code: "" });
+   };
+
+   // Press Email Certification Code
+   const isRightEmailCode = () => {
+      if (!inputState.email_code) return;
+      console.log(inputState.email_code);
+      setInputState({ ...inputState, is_right_email: true });
+   };
+
+   /* 약관 설정 관련 */
+
+   // '보기' 버튼클릭 시 모달창 띄우기
+   const setTerms = async (key) => {
+      if (!inputState[key].contents) {
+         const data = await fetch(terms[key]);
+         const result = await data.text();
+         setInputState({ ...inputState, [key]: { ...inputState[key], contents: result }, selected_term: key });
+      } else {
+         setInputState({ ...inputState, selected_term: key });
+      }
+   };
+
+   // 약관 상세조회 모달창 닫기
+   const closeModal = (is_apply) => {
+      const key = inputState.selected_term;
+      setInputState({ ...inputState, selected_term: null, [key]: { ...inputState[key], is_apply: is_apply || inputState[key].is_apply } });
    };
 
    /* 정규식 관련 */
@@ -73,7 +112,7 @@ const SignupLogic = ({ history }) => {
       return str.replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
    };
 
-   return { inputState, setInfo, signup, checkPwForm, setPhoneForm };
+   return { inputState, setInfo, signup, checkPwForm, sendEmail, isRightEmailCode, setTerms, closeModal };
 };
 
 export default SignupLogic;
